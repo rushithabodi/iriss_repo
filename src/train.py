2595701e6a49bcbfeff0d32546d4f81b0bf07e0d
rushithabodi <rushithabodi@gmail.com>
2025-07-09 12:04:00 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,16 +1,23 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 import joblib
 
-# Load data
-df = pd.read_csv('data/heart.csv')
-X = df.drop('target', axis=1)
-y = df['target']
 
-# Train model
-model = RandomForestClassifier()
-model.fit(X, y)
+df = pd.read_csv('iris.data', header=None)
+df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
 
-# Save model
-joblib.dump(model, 'model/model.pkl')
-print(" Heart disease model trained.")
+
+X = df.drop('class', axis=1)
+y = df['class']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+
+joblib.dump(model, 'iris_model.pkl')
+print("✅ Iris model trained and saved as 'iris_model.pkl'")
